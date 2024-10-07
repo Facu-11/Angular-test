741a8b44f2b60c26e8231feafb0a452a874d5b87
Facu-11 <fphatouros2@gmail.com>
2024-10-07 00:31:49 -0300

    7/10
    Banner configurado, y nuestros productos sin terminar

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,14 +1,45 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 
 export class BannerComponent {
-  
+  images = [
+    'https://picsum.photos/400/400?random=1',
+    'https://picsum.photos/400/400?random=1',
+    'https://picsum.photos/400/400?random=1',
+    
+  ];
+  currentIndex = 0;
+
+  title = 'Apple Watch';
+  subtitle = 'The ultimate device for a healthy life';
+
+
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  getBackgroundImage() {
+    return `url(${this.images[this.currentIndex]})`;
+  }
+
+  // startAutoSlide() {
+  //   setInterval(() => {
+  //     this.nextSlide();
+  //   }, 10000); // Cambiar el banner cada 10 segundos
+
+
 }
