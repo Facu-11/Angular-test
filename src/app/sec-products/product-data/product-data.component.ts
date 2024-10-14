7ccdd2ef33ecbe54d082d61a8f53b7077de7014d
Facu-11 <fphatouros2@gmail.com>
2024-10-14 14:02:42 -0300

    seccion product
    comienzo de la seccion

---
+++ src/app/sec-products/product-data/product-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-data.component.html',
+  styleUrl: './product-data.component.css'
+})
+export class ProductDataComponent {
+
+}
