d2c7edd202b8080f556f3a4ee1485db49968cd6f
GabrielOsOj <cuentaauxiliar2@hotmail.com>
2024-11-05 00:22:39 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { ComparatorComponentComponent } from './phoneComparator-page/comparator-
 import { HomeComponent } from './falsoHome/home.component';
 import { ProductContainerComponent } from './sec-products/product-container/product-container.component';
 import { ProductGalleryComponent } from './product/product-gallery/product-gallery.component';
-import { ProductPageComponent } from './product/product-main-page/product-page.component';
+import { ProductPageComponent } from './product/product-main-gae/product-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: "comparador", component: ComparatorComponentComponent },
     { path: "testZone", component: ProductContainerComponent },
     { path: "product", component: ProductGalleryComponent},
+<<<<<<< Updated upstream
     { path: "product2", component:ProductPageComponent },
 
 ]
